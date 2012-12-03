8fbf9a7f3ffbce6a792e33cda48b7b916d0cfae5
Robert Kroeger <rjkroege@chromium.org>
2012-12-03 07:22:15 -0500

    working version

--- wikinew/main.go
+++ wikinew/main.go
@@ -6,6 +6,9 @@ import (
     "log"
    "time"
     "strings"
+    "text/template"
+    "bufio"
+    "os/exec"
 )
 
 
@@ -18,11 +21,11 @@ import (
 
 type Handler func([]string)
 
-// TODO(rjkroege): I can refactor this with the other component.
+// TODO(rjkroege): I can refactor this with the other tool.
 type Article struct {
     Filename string
     Title string
-    Date time.Time
+    PrettyDate string
 }
 
 func filter(r rune) rune {
@@ -38,47 +41,87 @@ func filter(r rune) rune {
     return nr
 }
 
-func makearticle(args []string) *Article {
+func Makearticle(args []string) *Article {
     s := strings.Join(args, " ");
-    a := Article{ strings.Map(filter, s) + ".md", s, time.Now()}
+    a := Article{ strings.Map(filter, s) + ".md", s, time.Now().Format(time.UnixDate)}
     return &a;
+}
+
+const (
+path = "/Users/rjkroege/Dropbox/wiki2/"
+
+// for debugging
+// path = "./"
+
+journaltmpl = 
+`title: {{.Title}}
+date: {{.PrettyDate}}
+
+Yo dawg! Write stuff here.
+`
+
+booktmpl =
+`title: {{.Title}}
+date: {{.PrettyDate}}
+tags: bib
+
+Yo dawg! Put the bookreview here.
+`
+
+)
+
+// Might want to read where Plan9 lives from the environment?
+// Add wikimake to the bar for this edit pain.
+// Might want to sdd support for connecting to plumber
+func (md *Article) Plumb() {
+    // TODO(rjkroege): make pathifying a method on md
+    err := exec.Command("/usr/local/plan9/bin/plumb", path + md.Filename).Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func (md *Article) Emit(tpl string) *Article {
+    pth := path + md.Filename
+    ofd, werr := os.OpenFile(pth, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0644);
+    f :=  template.Must(template.New("footer").Parse(tpl));
+    defer ofd.Close()
+    if werr != nil {
+        log.Fatal("couldn't open", pth)
+    }
+    w := bufio.NewWriter(ofd)
+    defer w.Flush()
+    f.Execute(w, md)
+    return md
 }
 
 func journal(args []string) {
     fmt.Print("setup a new journal article", args, "\n");
-    a := makearticle(args)
-    fmt.Print(a, "\n");
+   Makearticle(args).Emit(journaltmpl).Plumb()
+    // TODO(rjkroege): figure out how to open it with plumber here.
 }
 
 func book(args []string) {
     fmt.Print("setup a new book review", args, "\n");
-    a := makearticle(args) 
-    fmt.Print(a, "\n");
+   Makearticle(args).Emit(booktmpl).Plumb()
 }
 
+// TODO(rjkroege): use @foo as a tag that goes in the tags entry (to create trails or the like)
+// TODO(rjkroege): add usage output
 func main() {
-    fmt.Print("hello world\n");
-
     handlers := map[string]Handler{
         "journal": journal,
         "book": book }
-
-    // debugging...
-    fmt.Print("length", len(os.Args), "\n");
-    for i, _ := range(os.Args) {
-        fmt.Print("> ", os.Args[i], "\n");
-    }
 
     if len(os.Args) < 2 {
         log.Fatal("Not enough arguments\n");
     }
 
-     f, ok := handlers[os.Args[1]];
+    f, ok := handlers[os.Args[1]];
     if !ok {
         log.Fatal("Unsupported sub-command\n");
     }
 
      f(os.Args[2:]);
-  
 }
 
