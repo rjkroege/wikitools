8d3f175b2471cb3fdf2b767c304a87ebbaf9a381
Robert Kroeger <rjkroege@chromium.org>
2013-08-13 07:51:09 -0400

    added bibliography tags to book templates

--- wiki/templates.go
+++ wiki/templates.go
@@ -13,8 +13,32 @@ booktmpl =
 `title: {{.Title}}
 date: {{.DetailedDate}}
 tags: {{.Tagstring}}
+bib-address:
+bib-author:
+bib-booktitle: {{.Title}}
+bib-chapter:
+bib-edition:
+bib-editor:
+bib-eprint:
+bib-howpublished:
+bib-institution:
+bib-journal:
+bib-month:
+bib-number:
+bib-organization:
+bib-pages:
+bib-publisher:
+bib-school:
+bib-series:
+bib-title: {{.Title}}
+bib-type:
+bib-url:
+bib-volume:
+bib-year:
+bib-isbn:
+bib-dewey:
 
-Yo dawg! Put the bookreview here.
+Yo dawg! Put the bookreview here. Delete the undesired tags.
 `
 
 )
