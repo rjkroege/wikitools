3d31caa94d92c83618d728cdd9a1e0b2cca4394b
Robert Kroeger <rjkroege@liqui.org>
2020-08-31 07:47:07 -0400

    First cut at parse
    The new wiki system needs a pre-processor to find the links etc. This
    is a starting point for that feature that parses a single markdown
    file and dumps info about it.

--- cmd/wikipp/main.go
+++ cmd/wikipp/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/rjkroege/wikitools/wiki"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+	"github.com/yuin/goldmark/text"
 )
 
 var (
+	// true for now for testing. But put it back.
 	testlog = flag.Bool("testlog", false,
 		"Log in the conventional way for running in a terminal. Also changes where to find the configuration file.")
 )
@@ -21,18 +29,59 @@ func main() {
 		defer wiki.LogToTemp()()
 	}
 
+	// for testing
+	log.SetOutput(os.Stderr)
+
 	log.Println("foo bar")
 
-	// parse. [do I want to parse or just be hacky with regexps?] If I hack it... things will soon not work.
-	// update the database (can happen asynchronously)
-	// modify (e.g. custom fenced blocks made into pictures), link maps
-	// emit (html or markdown?)
-
-	// pretend to be cat.
-	if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
-		log.Println("can't pretend to be cat:", err)
+	// 0. read
+	mdf, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Println("can't read Stdin:", err)
+		if _, err := io.WriteString(os.Stdout, "NOCUSTOM"); err != nil {
+			// We've done our best so give up.
+			log.Fatalf("giving up, can't write to Stdout: %v", err)
+		}
 	}
 
-	// Add a string to the doc.
-	fmt.Printf("\n---\n# Hello World\n")
+	// 1. Make a parser.
+	// TODO(rjk): what extensions do I need?
+	md := goldmark.New(
+		goldmark.WithExtensions(
+			extension.GFM,
+			meta.Meta,
+		),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+			html.WithXHTML(),
+		),
+	)
+	context := parser.NewContext()
+
+	// 2. Parse
+	reader := text.NewReader(mdf)
+	docroot := md.Parser().Parse(reader, parser.WithContext(context))
+
+	// experiment
+	// TODO(rjk): metadata extraction failed
+	// TODO(rjk): parse math data
+	// TODO(rjk): extract the outbound links
+	metaData := meta.Get(context)
+	title := metaData["title"]
+	log.Println(title)
+
+	// 3. Update the document and stored state.
+	// TODO(rjk): Writing an extension is probably the right way.
+	ast.Walk(docroot, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
+		log.Println("node:", n.Type(), "kind:", n.Kind())
+		return ast.WalkContinue, nil
+	})
+
+	// 4. Generate HTML
+	if err := md.Renderer().Render(os.Stdout, mdf, docroot); err != nil {
+		log.Fatalf("can't write: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	9fans.net/go v0.0.2
 	github.com/knieriem/markdown v0.0.0-20140730233915-325ec6c40dd4
 	github.com/sanity-io/litter v1.2.0
+	github.com/yuin/goldmark v1.2.1
+	github.com/yuin/goldmark-meta v0.0.0-20191126180153-f0638e958b60
 )
