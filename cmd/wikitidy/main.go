31dbe0dd27be0f5b9f77b36c32afc5b3ef1d5b6c
Robert Kroeger <rjkroege@liqui.org>
2020-09-14 09:26:09 -0400

    Code cleanup in wikitidy
    Remove duplicated code in wikitidy command.

--- cmd/wikitidy/main.go
+++ cmd/wikitidy/main.go
@@ -16,60 +16,32 @@ var reportflag = flag.Bool("report", false, "Generate the metadata status report
 
 func main() {
 	flag.Parse()
-
-	// TODO(rjk): This mainline can be refactored nicely..
-	if *reportflag {
-		log.Println("do report")
-		report, err := article.MakeMetadataReporter()
+	
+	// The default Tidying implementation can always be created without error.
+	tidying, err := article.MakeFilemover(*dryrun)
+	switch {
+	case *reportflag:
+		tidying, err = article.MakeMetadataReporter()
 		if err != nil {
-			log.Fatal("No BatchCleaner:", err)
+			log.Fatal("No MetadataReporter:", err)
 		}
-
-		if err := filepath.Walk(wiki.Basepath, func(path string, info os.FileInfo, err error) error {
-			return report.EachFile(path, info, err)
-		}); err != nil {
-			log.Fatal("report walk: ", err)
-		}
-
-		if err := report.Summary(); err != nil {
-			log.Fatal("report Summary: ", err)
-		}
-		os.Exit(0)
-	}
-
-	if *deepclean {
-		// TODO(rjk): Finish this.
-		log.Println("do deepclean")
-		update, err := article.MakeMetadataUpdater()
+	case *deepclean:
+		tidying, err = article.MakeMetadataUpdater()
 		if err != nil {
 			log.Fatal("No MetadataUpdater:", err)
 		}
-
-		if err := filepath.Walk(wiki.Basepath, func(path string, info os.FileInfo, err error) error {
-			return update.EachFile(path, info, err)
-		}); err != nil {
-			log.Fatal("deepclean walk: ", err)
-		}
-		if err := update.Summary(); err != nil {
-			log.Fatal("report Summary: ", err)
-		}
-		os.Exit(0)
 	}
 
 	// Default function now will relocate all files in the non-special
 	// directories. TODO(rjk): Consider better command line structure. Surely
 	// there's a package to do this for me.
 
-	mover, err := article.MakeFilemover(*dryrun)
-	if err != nil {
-		log.Fatal("No MakeFilemover:", err)
-	}
 	if err := filepath.Walk(wiki.Basepath, func(path string, info os.FileInfo, err error) error {
-		return mover.EachFile(path, info, err)
+		return tidying.EachFile(path, info, err)
 	}); err != nil {
 		log.Fatal("mover walk: ", err)
 	}
-	if err := mover.Summary(); err != nil {
+	if err := tidying.Summary(); err != nil {
 		log.Fatal("report Summary: ", err)
 	}
 	os.Exit(0)
