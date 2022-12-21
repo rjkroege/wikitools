e3de4cb39aae527d65eae942ee74a9e406ee028f
Robert Kroeger <rjkroege@liqui.org>
2022-12-21 16:59:18 -0500

    Refactor command dispatch
    Refactor the command dispatch to align with the intended command
    structure and prepare for the addition of new commands.

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 	"github.com/rjkroege/wikitools/cmd"
 	"github.com/rjkroege/wikitools/corpus"
 	"github.com/rjkroege/wikitools/wiki"
+	"github.com/rjkroege/wikitools/corpus/tidy"
 )
 
 var CLI struct {
 	// TODO(rjk): Move the default location.
+	// TODO(rjk): Move these into settings.
 	ConfigFile string `type:"path" help:"Set alternate configuration file" default:"~/.wikinewrc"`
 	Debug      bool   `help:"Enable debugging conveniences as needed."`
+	Dryrun    bool `help:"Don't actually modify anything but instead just show what would happen."`
 
 	// Might have a different command for Alfred vs Non-alfted case?
 	New struct {
@@ -20,26 +23,31 @@ var CLI struct {
 	} `cmd help:"Create new wiki article"`
 
 	Preview struct {
+		// TODO(rjk): Need to make this better.
 		Article string `arg:"" name:"article" type:"path" help:"Article to preview"`
 	} `cmd help:"Preview a wiki article"`
 
 	Tidy struct {
-		Dryrun    bool `help:"Don't actually move the files, just show what would happen"`
-		Deepclean bool `help:"Rewrite the metadata, move files into improved directories"`
-		Report    bool `help:"Generate the metadata status report."`
+		// TODO(rjk): Need to write this.
+		All struct{} `cmd help:"Do all possible tidying."  default:"1"`
+		Deepclean struct {} `cmd help:"Modernize the metadata, fix links, etc."`
+		Move struct {} `cmd help:"Move files into the correct places."`
+		Findersync  struct {}  `cmd help:"Sync metadata info the Spotlight metadata attributes."`
 	} `cmd help:"Clean up wiki aritcles: right structure, corrected metadata, etc."`
 
-	List struct {
-	} `cmd help:"List all articles using the corpus facilities"`
+	Report struct {
+		Articles struct {} `cmd help:"List all articles." default:"1"`
+		// TODO(rjk): where it puts it should be configurable right?
+		Metadata struct {} `cmd help:"List the metadata versions of each article."`
+		Tags struct {} `cmd help:"List all of the in-use tags."`
+		Todos struct {} `cmd help:"List all outstanding TODO items."`
+	} `cmd help:"Generate reports on the wiki corpus."`
 
 	Bearimport struct {
 		Outputdir string `help:"Output directory for importable files" type:"path" default:"./converted"`
 
 		Filestoprocess []string `arg:"" name:"filestoprocess" help:"List of article tags and its title" type:"path"`
 	} `cmd help:"Reprocess articles out of Bear for wiki"`
-
-	Test struct {
-	} `cmd help:"Stub action, does nothing."`
 }
 
 func main() {
@@ -62,13 +70,50 @@ func main() {
 	case "bearimport <filestoprocess>":
 		log.Println("should run Bearimport here", CLI.Bearimport.Filestoprocess)
 		cmd.Bearimport(settings, CLI.Bearimport.Outputdir, CLI.Bearimport.Filestoprocess)
-	case "tidy":
-		log.Println("should run Tidy here")
-		cmd.Tidy(settings, CLI.Tidy.Dryrun, CLI.Tidy.Deepclean, CLI.Tidy.Report)
-	case "test":
-		log.Println("Got a test command")
-	case "list":
-		log.Println("Got a test command")
+
+	case "tidy all":
+		log.Println("tidy all not implemented")
+		// TODO(rjk): Union the other operations.
+	case "tidy deepclean":
+		// TODO(rjk): Highly likely that this needs some kind of settings. 
+		tidying, err := tidy.NewMetadataUpdater()
+		if err != nil {
+			log.Fatal("Can't make a MetadataUpdater( because:", err)
+		}
+		if err := corpus.Everyfile(settings, tidying); err != nil {
+			log.Fatal(err)
+		}
+		if err := tidying.Summary(); err != nil {
+			log.Fatal("report Summary: ", err)
+		}
+	case "tidy findersync":
+		log.Println("tidy findersync not implemented")
+		// TODO(rjk): Write me.
+	case "tidy move":
+		tidying, _ := tidy.NewFilemover(settings, CLI.Dryrun)
+		if err := corpus.Everyfile(settings, tidying); err != nil {
+			log.Fatal(err)
+		}
+		if err := tidying.Summary(); err != nil {
+			log.Fatal("report Summary: ", err)
+		}
+	case "report metadata":
+		tidying, err := tidy.NewMetadataReporter(settings)
+		if err != nil {
+			log.Fatal("Can't make a MetadataReporter because:", err)
+		}
+		if err := corpus.Everyfile(settings, tidying); err != nil {
+			log.Fatal(err)
+		}
+		if err := tidying.Summary(); err != nil {
+			log.Fatal("report Summary: ", err)
+		}
+	case "report tags":
+		log.Println("report tags not implemented")
+		// TODO(rjk): Write me.
+	case "report todos":
+		log.Println("report todos not implemented")
+	case "report articles":
 		tid := corpus.NewListAllWikiFilesTidying()
 		if err := corpus.Everyfile(settings, tid); err != nil {
 			log.Fatal(err)
