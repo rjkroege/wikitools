76fca5d293162114b6f2cdd31824a4363f200d60
Robert Kroeger <rjkroege@liqui.org>
2024-06-22 07:16:01 -0400

    Ergnomic in wikilink following
    Handle extensions correctly for wikilink following.
    Start logging issues in a more sensible way.

--- cmd/plumb.go
+++ cmd/plumb.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"path/filepath"
 
 	"github.com/rjkroege/wikitools/corpus/search"
 	"github.com/rjkroege/wikitools/wiki"
 	"github.com/rjkroege/gozen"
+	"9fans.net/go/acme"
 )
 
 func PlumberHelper(settings *wiki.Settings, lsd, wikitext string) {
@@ -15,12 +18,75 @@ func PlumberHelper(settings *wiki.Settings, lsd, wikitext string) {
 	// TODO(rjk): Consider renaming this later.
 	mapper := search.MakeWikilinkNameIndex()
 
-	// TODO(rjk): Trial code.
+	if filepath.Ext(wikitext) == "" {
+		wikitext = wikitext + ".md"
+	}
+
 	fp, err := mapper.Path(settings.Wikidir, lsd, wikitext)
 	if err != nil {
 		log.Printf("indexer.Path errored on %q: %v", wikitext, err)
+		woe := fmt.Sprintf("trying to follow [[%s]] failed: %v\n", wikitext, err)
+		writewikierror(settings, []byte(woe))
+		return
 	}
 
 	log.Println(fp)
 	gozen.Editinacme(fp)
+}
+
+
+const logfile = "+Wikierror"
+
+// writewikierror appends text to the special file for wiki errors.
+func writewikierror(settings *wiki.Settings, text []byte) error {
+	// shove stuff into Edwood/Acme error area
+	fn := filepath.Join(settings.Wikidir, logfile)
+	
+	// Two choices: we already have the Window open.
+	wins, err := acme.Windows()
+	if err != nil {
+		return fmt.Errorf("plumbhelper acme.Windows")
+	}
+
+	win := (*acme.Win)(nil)
+	for _, wi := range wins {
+		if wi.Name == fn {
+			win, err = acme.Open(wi.ID, nil)
+			if err != nil {
+				return fmt.Errorf("plumbhelper acme.Open")
+			}
+			break
+		}
+	}
+
+	if win == nil {
+		var err error
+		win, err = acme.New()
+		if err != nil {
+			return fmt.Errorf("writewikierror acme.New: %v", err)
+		}
+
+		if err := win.Ctl("nomark"); err != nil {
+			return fmt.Errorf("writewikierror win.Ctl nomark: %v", err)
+		}
+
+		if err := win.Name(fn); err != nil {
+			return fmt.Errorf("writewikierror win.Name: %v", err)
+		}
+
+		if err = win.Ctl("mark"); err != nil {
+			return fmt.Errorf("writewikierror %q: %v", "mark", err)
+		}
+
+		if err = win.Ctl("clean"); err != nil {
+			return fmt.Errorf("writewikierror %q: %v", "clean", err)
+		}
+	}
+
+	// TODO(rjk): This code could be more ergonomic.
+	if _, err := win.Write("body", text); err != nil {
+		return fmt.Errorf("writewikierror %q: %v", "write", err)
+	}
+
+	return nil
 }
