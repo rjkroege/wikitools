1705abca1ff09f7e0a551860bab63201336e9460
Robert Kroeger <rjkroege@liqui.org>
2024-06-30 04:14:32 -0600

    Utility code to read/write backlinks to a file

--- article/write_backlinks_test.go
+++ article/write_backlinks_test.go
@@ -1,18 +1,20 @@
 package article
 
 import (
-	"testing"
-	"path/filepath"
 	"os"
+	"path/filepath"
+	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/rjkroege/wikitools/corpus"
+	// "github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestWriteBacklinks(t *testing.T) {
 	td := t.TempDir()
 
 	fpath := filepath.Join(td, "testfile")
-	
+
 	wikilink := corpus.MakeWikilink("jiminy", "cricket")
 	backmap := make(map[corpus.Wikilink]corpus.Empty)
 	backmap[wikilink] = corpus.Empty{}
@@ -25,7 +27,29 @@ func TestWriteBacklinks(t *testing.T) {
 		t.Fatalf("can't write xattr to %q: %v", fpath, err)
 	}
 
-	// TODO(rjk): Decode and compare
-		
-}
+	gotmap, err := ReadBacklinks(fpath)
+	if err != nil {
+		t.Fatalf("can't read xattr from %q: %v", fpath, err)
+	}
 
+	if diff := cmp.Diff(backmap, gotmap); diff != "" {
+		t.Errorf("[%d] dump mismatch (-want +got):\n%s", 0, diff)
+	}
+
+	wikilink2 := corpus.MakeWikilink("hello", "xx/foo/bar")
+	backmap[wikilink2] = corpus.Empty{}
+
+	if err := WriteBacklinks(fpath, backmap); err != nil {
+		t.Fatalf("can't second write xattr to %q: %v", fpath, err)
+	}
+
+	gotmap, err = ReadBacklinks(fpath)
+	if err != nil {
+		t.Fatalf("can't read xattr from %q: %v", fpath, err)
+	}
+
+	if diff := cmp.Diff(backmap, gotmap); diff != "" {
+		t.Errorf("[%d] dump mismatch (-want +got):\n%s", 1, diff)
+	}
+
+}
