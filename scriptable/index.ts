9d346dda66efb44601a324c8b6f2730ab5874a02
Robert Kroeger <rjkroege@liqui.org>
2024-07-21 11:06:38 -0400

    Added a wip JavaScript module for the iOS Scriptable Support

---
+++ scriptable/index.ts
@@ -0,0 +1,7 @@
+import ForNow from "./paths.js";
+
+
+
+console.log("Hello via Bun!");
+
+console.log(ForNow());
---
+++ scriptable/paths_test.js
@@ -0,0 +1,14 @@
+// [bun test – Test runner | Bun Docs](https://bun.sh/docs/cli/test)
+import { expect, test } from "bun:test";
+import { forNowImpl } from "./paths.js";
+
+
+test("1995-12-17T03:24:00", () => {
+  expect(forNowImpl(new Date("1995-12-17T03:24:00"))).toBe("1995/12-Dec/17");
+});
+
+test("1995-03-17T03:24:00", () => {
+  expect(forNowImpl(new Date("1995-03-17T03:24:00"))).toBe("1995/03-Mar/17");
+});
+
+
