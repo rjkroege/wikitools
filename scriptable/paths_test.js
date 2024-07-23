66d585dae351ee23057ec0fe060fd60a6e52ed63
Robert Kroeger <rjkroege@liqui.org>
2024-07-23 05:57:05 -0400

    Added pretty-printer, pretty-printed.

--- scriptable/paths_test.js
+++ scriptable/paths_test.js
@@ -2,7 +2,6 @@
 import { expect, test } from "bun:test";
 import { forNowImpl } from "./paths.js";
 
-
 test("1995-12-17T03:24:00", () => {
   expect(forNowImpl(new Date("1995-12-17T03:24:00"))).toBe("1995/12-Dec/17");
 });
@@ -10,5 +9,3 @@ test("1995-12-17T03:24:00", () => {
 test("1995-03-17T03:24:00", () => {
   expect(forNowImpl(new Date("1995-03-17T03:24:00"))).toBe("1995/03-Mar/17");
 });
-
-
