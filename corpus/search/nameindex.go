37b3cdf9ac53be21532482e9fb6b6defa8e82660
Robert Kroeger <rjkroege@liqui.org>
2025-09-05 08:01:46 -0400

    Remove darwin split in search
    Without Spotlight use, ,don't need the darwin split in the
    corpus/search code so remove that.

--- corpus/search/nameindex.go
+++ corpus/search/nameindex.go
@@ -1,34 +1,80 @@
-//go:build !darwin
-// +build !darwin
-
-// All non-darwin platforms as darwin
-
 package search
 
 import (
 	"fmt"
-	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/rjkroege/wikitools/corpus"
 )
 
-type portableWikilinkNameIndex struct {
+// spotlightWikilinkIndexer hides all of the darwin-specific code needed
+// to support indexing using spotlight.
+// Changes to this structure need to be synchronized correctly.
+type spotlightWikilinkIndexer struct {
+	wikiroot string
 }
 
-var _ corpus.LinkToFile = (*portableWikilinkNameIndex)(nil)
+var _ corpus.LinkToFile = (*spotlightWikilinkIndexer)(nil)
 
-func (_ *portableWikilinkNameIndex) Path(_, _, _ string) (string, error) {
-	return "", fmt.Errorf("portableWikilinkNameIndex.Path not implemented")
-}
-func (_ *portableWikilinkNameIndex) Allpaths(_, _, _ string) ([]string, error) {
-	return nil, fmt.Errorf("portableWikilinkNameIndex.Allpaths not implemented")
+func (spix *spotlightWikilinkIndexer) Path(location, lsd, wikitext string) (string, error) {
+	basepart := filepath.Base(wikitext)
+	if basepart == "" {
+		return "", EmptyWikitextFile
+	}
+
+	allpaths, err := spix.pathsforwikitext(location, basepart)
+	if err != nil {
+		return "", err
+	}
+
+	return disambiguatewikipaths(location, lsd, wikitext, allpaths)
 }
 
-func MakeWikilinkNameIndex(_ string) *portableWikilinkNameIndex {
-	log.Println("hi not from darwin")
-	return &portableWikilinkNameIndex{}
+func (_ *spotlightWikilinkIndexer) Allpaths(location, lsd, wikitext string) ([]string, error) {
+	return nil, fmt.Errorf("StubLinkToFile not implemented")
 }
 
-func (_ *portableWikilinkNameIndex) Wikitext(_, _ string) (string, error) {
-	return "", fmt.Errorf("portableWikilinkNameIndex.Wikitext not implemented")
+// pathsforwikitext returns all the absolute paths for resources in directory tree specified by
+// location with leaf path wikitextfile.
+// TODO(rjk): the input text might or might not have a file name extension. I'm currently
+// not clear about that.
+// TODO(rjk): does it need an object?
+func (_ *spotlightWikilinkIndexer) pathsforwikitext(location, wikitextfile string) ([]string, error) {
+    matches :=  []string{}
+    err := filepath.WalkDir(location, func(path string, d os.DirEntry, err error) error {
+        if err != nil {
+            return nil
+        }
+        if filepath.Base(path) == wikitextfile {
+            abs, err := filepath.Abs(path)
+            if err != nil {
+                return err
+            }
+            matches = append(matches, abs)
+        }
+        return nil
+    })
+    return matches, err
+}
+
+func MakeWikilinkNameIndex(wikiroot string) *spotlightWikilinkIndexer {
+	// The Apple docs imply (very strongly) that there can only be a single
+	// query running at a time. Remember this if I should convert the tidy
+	// code to run concurrently.
+	spidx := &spotlightWikilinkIndexer{
+		wikiroot: wikiroot,
+	}
+	return spidx
+}
+
+// make suffix stripping configurable. I expect that I'd want svg etc to keep its
+// suffix?
+func (spix *spotlightWikilinkIndexer) Wikitext(frompath, topath string) (string, error) {
+	// Find allz of the paths
+	allpaths, err := spix.pathsforwikitext(filepath.Dir(frompath), filepath.Base(topath))
+	if err != nil {
+		return "", fmt.Errorf("Wikitext pathsforwikitext %w", err)
+	}
+	return buildshortestwikitext(spix.wikiroot, topath, allpaths)
 }
