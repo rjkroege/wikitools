3905c8d6e69b78c4b434ae5a4bcd78684e6d118c
Robert Kroeger <rjkroege@liqui.org>
2025-09-24 07:39:13 -0400

    Better Wikitext test
    Add a much more complete Wikitext test that exercises multiple
    interesting test cases from real (i.e. not mock) index.

--- corpus/search/nameindex_test.go
+++ corpus/search/nameindex_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 type nameIndexTestVector struct {
@@ -77,3 +78,77 @@ func TestSplitPathParts(t *testing.T) {
 	}
 }
 
+type WikitextTestCase struct {
+	frompath string
+	topath string
+	want string
+	wanterr error
+}
+
+func TestWikitext(t *testing.T) {
+	bp, err := filepath.Abs("testdata")
+	if err != nil {
+		t.Fatalf("test can't run: %v", err)
+	}
+
+	spix := MakeWikilinkNameIndex(bp)
+
+	tests := []WikitextTestCase{
+		{
+			frompath: "wiki/unsorted/Saturday.md",
+			topath: "wiki/unsorted/EveningJournal.md",
+			want: "EveningJournal.md",
+			wanterr: nil,
+		},
+		{
+			frompath: "wiki/unsorted/Saturday.md",
+			topath: "wiki/2023/02-Feb/28/Saturday.md",
+			want: "28/Saturday.md",
+			wanterr: nil,
+		},
+		{
+			frompath: "wiki/04-Apr/20/Thursday.md",
+			topath: "wiki/04-Apr/24/Monday.md",
+			want: "24/Monday.md",
+			wanterr: nil,
+		},
+		{
+			frompath: "wiki/2023/06-Jun/5/Monday.md",
+			topath: "wiki/2023/06-Jun/Monday.md",
+			want: "06-Jun/Monday.md",
+			wanterr: nil,
+		},
+		{
+			frompath: "wiki/2023/06-Jun/Monday.md",
+			topath: "wiki/2023/06-Jun/5/Monday.md",
+			want: "5/Monday.md",
+			wanterr: nil,
+		},
+		{
+			frompath: "wiki/unsorted/EveningJournal.md",
+			topath: "wiki/2023/Decisions.md",
+			want: "2023/Decisions.md",
+			wanterr: nil,
+		},
+		{
+			frompath: "wiki/2023/06-Jun/Monday.md",
+			topath: "wiki/2023/12-Dec/1/PlottingTools.md",
+			want: "12-Dec/1/PlottingTools.md",
+			wanterr: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := spix.Wikitext(
+			filepath.Join(bp, tc.frompath), 
+			filepath.Join(bp, tc.topath))
+
+		want := tc.want
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("path mismatch (-want +got):\n%s", diff)
+		}
+		if diff := cmp.Diff(tc.wanterr, err, cmpopts.EquateErrors()); diff != "" {
+			t.Errorf("error mismatch (-want +got):\n%s", diff)
+		}
+	}
+}
