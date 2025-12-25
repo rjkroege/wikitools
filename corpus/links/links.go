69f36e701a3a1dd2800afd911fbd3f65d06580e1
Robert Kroeger <rjkroege@liqui.org>
2025-12-25 07:45:57 -0700

    Add LinkTuple struct to links.go

--- corpus/links/links.go
+++ corpus/links/links.go
@@ -13,6 +13,12 @@ import (
 
 type empty = struct{}
 
+// LinkTuple is a pair of string and corpus.WikilinkMap
+type LinkTuple struct {
+	From string
+	To   corpus.LinkMap[corpus.Wikilink]
+}
+
 type Links struct {
 	// A set of outgoing wikitext links from each fullpath-specified article.
 	ForwardLinks map[string]corpus.LinkMap[corpus.Wikilink]
